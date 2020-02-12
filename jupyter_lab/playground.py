b853cae083e99d01e972540f33e88165549dc17d
gmihaila <georgemihaila20@yahoo.com>
2020-02-12 12:14:47 -0600

    working towards updates

--- jupyter_lab/playground.py
+++ jupyter_lab/playground.py
@@ -28,6 +28,8 @@ DB_NAME = 'jupyter_talon_usage.db'
 # if user re-logs in timeframe it will reset idle time
 JUPYTER_HPC_TIMEOUT = 60    #seconds 15 default
 
+PYTHON_PATH = '/cm/shared/utils/PYTHON/3.6.5'
+CONDA_PATH = '/cm/shared/utils/PYTHON/ANACONDA/5.2'
 
 def logger(user, message, level, fname='logs.log', verbose=True, extra_log=True):
     """Logging function
@@ -66,30 +68,27 @@ def logger(user, message, level, fname='logs.log', verbose=True, extra_log=True)
     return
 
 
-def forward_jupyter(euid, passw, addrs, remote_port, local_port, running_pid, timeout=10):
+def jupyter_forward(euid, passw, addrs, remote_port, local_port, running_pid, timeout=10):
     """Forward port of running instance from HPC to local VM.
+    Start new Jupyter instance if nothing running.
     WITH A RUNNING JUPYTER INSTANCE ON A KNOWN PORT:
         - FORWARD KNOWN PORT TO GATEWAY
         - LOGIN IS SUCCESSFULL
         - LOGIN FAILED
+    Update users state to 'initialized' before running this function.
 
     Args:
         euid: User id.
         passw: Password for login to HPC.
         addrs: Hostname of HPC.
+        remote_port: Port used on HPC.
         local_port: Port on local VM gateway used to forward.
-        talon_port: Port on HPC needed to forward.
         running_pid: Pid of running process that does ssh tunneling.
         timeout: Seconds until end session.
     """
 
-    # SHELL COMMANDS
-    shell_change_path = "cd /storage/scratch2/%s"%(euid)
-    shell_ipy_dir = "export IPYTHONDIR=/storage/scratch2/%s/.ipython"%(euid)
-    shell_jupyter_config_dir = "export JUPYTER_CONFIG_DIR=/storage/scratch2/%s/.jupyter"%(euid)
-    shell_jupyter_data_dir = "export JUPYTER_DATA_DIR=/storage/scratch2/%s/.jupyter"%(euid)
-    shell_module_load = "module load anaconda"
-
+    # GRAB CURRENT REMOTE PORT FROM DB
+    current_remote_port = None
     logger(user=euid, message='FORWARD RUNNING JUPYTER INSTANCE PORT', level='INFO')
     child = pexpect.spawn('ssh -L 0.0.0.0:%s:127.0.0.1:%s  %s@%s -o StrictHostKeyChecking=no' %
                           (local_port, remote_port, euid, addrs), encoding='utf-8', timeout=timeout, logfile=None)
@@ -103,7 +102,8 @@ def forward_jupyter(euid, passw, addrs, remote_port, local_port, running_pid, ti
         try:
             child.expect('\n')
             out_line = child.before
-            print("out_line", out_line)
+            logger(user=euid, message=str(out_line), level='INFO')
+            # print("out_line", out_line)
 
             # FIRST LINE OF THE LOGIN
             if (" " in out_line) and not first_line:
@@ -112,19 +112,24 @@ def forward_jupyter(euid, passw, addrs, remote_port, local_port, running_pid, ti
             # LOGIN IS SUCCESSFULL
             if ("Last login:" in out_line) and first_line:
                 is_logged = True
-                logger(
-                    user=euid, message='\tLOGIN IS SUCCESSFULL! FORWARDING...', level='CRITICAL')
+                logger(user=euid, message='LOGIN IS TO HPC SUCCESSFULL! START JUPYTER...', level='CRITICAL')
                 # CHANGE PATH
-                child.sendline(shell_change_path)
-                # # ADD ENVIROMENT VARIABLES
-                child.sendline('export PATH="$PATH:/cm/shared/utils/PYTHON/3.6.5/bin"')
-                child.sendline('export PATH="$PATH:/cm/shared/utils/PYTHON/3.6.5/lib"')
-                child.sendline('export PATH="$PATH:/cm/shared/utils/PYTHON/ANACONDA/5.2/bin"')
-                child.sendline('export PATH="$PATH:/cm/shared/utils/PYTHON/ANACONDA/5.2/lib"')
-                child.sendline(shell_ipy_dir)
-                child.sendline(shell_jupyter_config_dir)
-                child.sendline(shell_jupyter_data_dir)
-                child.sendline('jupyter lab --no-browser --ip=0.0.0.0 --port=%s'%remote_port)
+                child.sendline("cd /storage/scratch2/%s"%euid)
+                # ADD ENVIROMENT VARIABLES
+                child.sendline('export PATH="$PATH:%s/bin"'%PYTHON_PATH)
+                child.sendline('export PATH="$PATH:%s/lib"'%PYTHON_PATH)
+                child.sendline('export PATH="$PATH:%s/bin"'%CONDA_PATH)
+                child.sendline('export PATH="$PATH:%s/lib"'%CONDA_PATH)
+                child.sendline("export IPYTHONDIR=/storage/scratch2/%s/.ipython"%euid)
+                child.sendline("export JUPYTER_CONFIG_DIR=/storage/scratch2/%s/.jupyter"%euid)
+                child.sendline("export JUPYTER_DATA_DIR=/storage/scratch2/%s/.jupyter"%euid)
+                if remote_port!=current_remote_port:
+                    # NEED NEW JUPYTER INSTANCE
+                    child.sendline('jupyter lab --no-browser --ip=0.0.0.0 --port=%s'%remote_port)
+            # JUPYTER STARTED SUCCESSFULLY
+            if ('The Jupyter Notebook is running at' in out_line) and is_logged and remote_port:
+                logger(user=euid, message='JUPYTER STARTED SUCCESSFULLY. LOCAL PORT: %s REMOTE PORT: %s'%
+                        (local_port, remote_port), level='CRITICAL')
                 # UPDATE USER DATABASE
                 # add_db(db_name=DB_NAME,
                 #        euid=euid,
@@ -165,5 +170,164 @@ def forward_jupyter(euid, passw, addrs, remote_port, local_port, running_pid, ti
             break
     return
 
+
+def jupyter_configure(euid, passw, addrs, running_pid, timeout=5):
+    """Jupyter configure and checking running instances.
+    if jupyter_last_port is same as remote_port from DB keep it.
+    if jupyter_last_port is same as remote_port from DB keep but other user is using, change it.
+    if jupyter_last_port is None in DB, create new one.
+    This function executes following procedures:
+        - Check if user exists.
+        Change path to scratch2.
+        Export enviroment variables (python and conda paths).
+        Run jupyter config command.
+
+    Args:
+        euid: User id.
+        passw: Password for login to HPC.
+        addrs: Hostname of HPC.
+        timeout: Seconds until end session.
+    Return:
+        is_user: True / Flase if user exists on HPC.
+        is_jupyter_config: True / Flase if user has proper jupyter configure file.
+        jupyter_last_port: Last running jupyter session port on HPC.
+    """
+
+    # GET REMOTE PORT FROM USER DB [IF OTHER USER IS USING IT RETURN None]
+    remote_port = None
+
+    logger(user=euid, message='CHECK IF ANY RUNNING JUPYTER and HASH AND JUPYTER CONFIG', level='INFO')
+    # UPDATE USER IN DATABASE
+    # add_db(db_name=DB_NAME,
+    #        euid=euid,
+    #        last_login=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    #        local_port=0,
+    #        talon_port=0,
+    #        login_node=LOGIN_NODE,
+    #        pid_session=running_pid,
+    #        state_session='initiated')
+
+    child = pexpect.spawn('ssh %s@%s -o StrictHostKeyChecking=no' %
+                          (euid, addrs), encoding='utf-8', timeout=timeout, logfile=None)
+    child.expect(['password: '])
+    child.sendline(passw)
+
+
+    shell_jupyter_config_path = "python -c \"import os; print('jupyter_config: ',os.path.exists('/storage/scratch2/%s/.jupyter/jupyter_notebook_config.json'))\""%(euid)
+    shell_jupyter_config_sha = "awk -F\": \" '/\"password\": /{print $2;}' /storage/scratch2/%s/.jupyter/jupyter_notebook_config.json"%(euid)
+    shell_jupyter_ports = "python -c 'import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); print(\"port_used\",s.connect_ex((\"localhost\", %s)) == 0)'"%(remote_port)
+
+    # STAUS VARIABLES
+    jupyter_last_port = None
+
+    # STATE BOOLEAN VARIABLES
+    is_jupyter_config = False
+    is_user = False
+    checking_running_jupyters = False
+    configuring_jupyter = False
+
+    # JUPYTER CONFIG FILE
+    allow_remote_access = "sed -i 's/#c.NotebookApp.allow_remote_access = False/c.NotebookApp.allow_remote_access = True/g' .jupyter/jupyter_notebook_config.py"
+    cull_busy = "sed -i 's/#c.MappingKernelManager.cull_busy = False/c.MappingKernelManager.cull_busy = False/g' .jupyter/jupyter_notebook_config.py"
+    cull_connected = "sed -i 's/#c.MappingKernelManager.cull_connected = False/c.MappingKernelManager.cull_connected = False/g' .jupyter/jupyter_notebook_config.py"
+    cull_idle_timeout = "sed -i 's/#c.MappingKernelManager.cull_idle_timeout = 0/c.MappingKernelManager.cull_idle_timeout = 10/g' .jupyter/jupyter_notebook_config.py"
+    cull_interval = "sed -i 's/#c.MappingKernelManager.cull_interval = 300/c.MappingKernelManager.cull_interval = 5/g' .jupyter/jupyter_notebook_config.py"
+    kernel_info_timeout = "sed -i 's/#c.MappingKernelManager.kernel_info_timeout = 60/c.MappingKernelManager.kernel_info_timeout = 60/g' .jupyter/jupyter_notebook_config.py"
+    shutdown_no_activity_timeout = "sed -i 's/#c.NotebookApp.shutdown_no_activity_timeout = 0/c.NotebookApp.shutdown_no_activity_timeout = %s/g' .jupyter/jupyter_notebook_config.py"%JUPYTER_HPC_TIMEOUT
+
+    # LOOP CHECK SHELL
+    while True:
+        try:
+            child.expect('\n')
+            out_line = child.before
+            # ONLY LOG
+            logger(user=euid, message=str(out_line),
+                   level='INFO', verbose=False)
+            # print("out_line", out_line)
+
+            # CHECK IF USER EXISTS
+            if "Last login:" in out_line:
+                # USER EXISTS
+                is_user = True
+                logger(user=euid, message='USER EXISTS!', level='INFO')
+                # CHANGE PATH
+                child.sendline("cd /storage/scratch2/%s"%euid)
+                # ADD ENVIROMENT VARIABLES
+                child.sendline('export PATH="$PATH:%s/bin"'%PYTHON_PATH)
+                child.sendline('export PATH="$PATH:%s/lib"'%PYTHON_PATH)
+                child.sendline('export PATH="$PATH:%s/bin"'%CONDA_PATH)
+                child.sendline('export PATH="$PATH:%s/lib"'%CONDA_PATH)
+                child.sendline("export IPYTHONDIR=/storage/scratch2/%s/.ipython"%euid)
+                child.sendline("export JUPYTER_CONFIG_DIR=/storage/scratch2/%s/.jupyter"%euid)
+                child.sendline("export JUPYTER_DATA_DIR=/storage/scratch2/%s/.jupyter"%euid)
+                # REMOVE POTENTIAL jupyter_notebook_config.py FILE
+                # child.sendline('rm  .jupyter')
+                # JUPYTER GENERATE CONFIG .jupyter
+                child.sendline("jupyter notebook --generate-config -y")
+                logger(user=euid, message='TRY TO CREATE jupyter_notebook_config.py', level='INFO')
+                # CHECK IF jupyter_notebook_config.json CREATED with
+                child.sendline(shell_jupyter_config_path)
+
+            # CHECK IF JUPYTER CONFIG EXISTS. KNOW THAT USER EXISTS
+            if ('Writing default config to:' in out_line) and is_user:
+                # SET PASSWORD
+                # CHANGE jupyter_notebook_config.py
+                is_jupyter_config = True
+                logger(user=euid, message='CREATED jupyter_notebook_config.py!', level='CRITICAL')
+                # CREATE JUPYTER PASSWORD
+                child.sendline("jupyter notebook password")
+                child.expect("Enter password:")
+                child.sendline(passw)
+                child.expect("Verify password:")
+                child.sendline(passw)
+
+            # CHANGE jupyter_notebook_config.py. KNOW THAT JUPYTER PASSWORD IS SET.
+            if ('Wrote hashed password to' in out_line) and is_jupyter_config:
+                logger(user=euid, message='JUPYTER PASSWORD IS SET!', level='CRITICAL')
+                # ALLOW REMOTE ACCESS
+                child.sendline(allow_remote_access)
+                # TIMEOUT KILL NOTEBOOK
+                child.sendline(cull_busy)
+                child.sendline(cull_connected)
+                child.sendline(cull_idle_timeout)
+                child.sendline(cull_interval)
+                child.sendline(kernel_info_timeout)
+                child.sendline(shutdown_no_activity_timeout)
+                logger(user=euid, message='CONFIGURED jupyter_notebook_config.py!', level='CRITICAL')
+                # CHECK IF PORT IS BEING USED
+                if remote_port is not None:
+                    child.sendline(shell_jupyter_ports)
+
+            # CHECK IF JUPYTER CONFIG NOT EXISTS. KNOW THAT USER EXISTS.
+            if ("('jupyter_config: ', False)" in out_line) and is_user:
+                # JUPYTER CONFIG NOT EXISTS AND USER EXISTS
+                logger(user=euid, message='JUPYTER FAILED TO CREATE CONFIG FILE!',level='ERROR')
+                logger(user=euid, message='FUNCTION ENDED!', level='ERROR')
+                child.close(force=True)
+                break
+
+            # CHECK IF PORT IS BEING USED
+            if ('port_used' in out_line):
+                if 'True' in out_line:
+                    jupyter_last_port = remote_port
+                    logger(user=euid, message="JUPYTER LAST PORT USED '%s'!"%jupyter_last_port,level='INFO')
+                    break
+                elif 'False' in out_line:
+                    # FIND NEW PORT ON TALON
+## ------HEREEEE----
+
+        except:
+            logger(user=euid, message='FUNCTION ENDED!', level='WARNING')
+            child.close(force=True)
+            break
+
+    if jupyter_last_port == "no_ports":
+        logger(user=euid, message='NO RUNNING JUPYTER NOTEBOOK', level='WARNING')
+    return is_user, is_jupyter_config, jupyter_last_port
+
+
+
 if __name__ == '__main__':
-    forward_jupyter(euid='gm0234', passw='', addrs=LOGIN_NODE, remote_port='39634', local_port='39634', running_pid='0', timeout=10)
+    is_user, is_jupyter_config, jupyter_last_port = jupyter_configure(euid='gm0234', passw='', addrs=LOGIN_NODE, remote_port='39634', running_pid=2, timeout=5)
+    print(is_user, is_jupyter_config, jupyter_last_port)
+    # forward_new_jupyter(euid='gm0234', passw='', addrs=LOGIN_NODE, remote_port='39634', local_port='39634', running_pid='0', timeout=10)
